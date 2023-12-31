751c7e1bf86cc75f4c3b06dec154b09abb5a6a5e
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2024-01-01 02:26:10 +0900

    Bump version

--- youtube_wpm/__version__.py
+++ youtube_wpm/__version__.py
@@ -4,6 +4,6 @@ from typing import Final
 __author__: Final = "Tsuyoshi Hombashi"
 __copyright__: Final = f"Copyright 2023, {__author__}"
 __license__: Final = "MIT License"
-__version__: Final = "0.0.3"
+__version__: Final = "0.1.0"
 __maintainer__: Final = __author__
 __email__: Final = "tsuyoshi.hombashi@gmail.com"
